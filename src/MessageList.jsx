dc5c9ad5614e436b5902b7c0cc2972cb418ad3cb
Shane Richman <Shane.M.Richman@gmail.com>
2017-11-01 03:19:51 +0000

    messages and usernames display with a propper key

--- src/MessageList.jsx
+++ src/MessageList.jsx
@@ -3,12 +3,17 @@ import Message from './Message.jsx';
 
 class MessageList extends Component {
   render() {
-    console.log(this.props);
+    const messages = this.props.messages.map(message => {
+      return <Message
+      key       ={message.id}
+      username  ={message.username}
+      content   ={message.content} />
+    });
     return (
       <main className="messages">
-        <Message />
-        <div className="message system">"
-          Anonymous1 changed their name to nomnom."
+          { messages }
+        <div className="message system">
+          "Anonymous1 changed their name to nomnom."
         </div>
       </main>
     );
