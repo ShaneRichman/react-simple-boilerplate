d150cb7a83a1dd2755efc5d3dc8209fa25fb14c1
Shane Richman <Shane.M.Richman@gmail.com>
2017-11-01 02:08:22 +0000

    initial commit and base app with timeout

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,37 @@
 import React, {Component} from 'react';
+import ChatBar from './ChatBar.jsx';
+import MessageList from './MessageList.jsx';
+
 
 class App extends Component {
+
+  constructor(props) {
+    console.log(props)
+    super(props);
+    console.log(this);
+    this.state = {loading: false};
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({loading: true})
+    }, 500)
+  }
+
   render() {
-    return (
-      <h1>Hello React :)</h1>
-    );
+    if(this.state.loading) {
+      return (
+        <div>
+          <nav className="navbar">
+            <a href="/" className="navbar-brand">Chatty</a>
+          </nav>
+          <MessageList />
+          <ChatBar />
+        </div>
+      );
+    } else {
+      return <h1>3 seconds have elapsed and page is loaded</h1>
+    }
   }
 }
 export default App;
