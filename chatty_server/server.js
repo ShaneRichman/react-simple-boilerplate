3540fa76aec3d7ffff4fa70e46c6f44799d6a7a0
Shane Richman <Shane.M.Richman@gmail.com>
2017-11-04 19:21:25 +0000

    set user colours on the server

--- chatty_server/server.js
+++ chatty_server/server.js
@@ -23,19 +23,39 @@ function broadcast(data) {
   });
 };
 
+function randomColour() {
+  const randomizer = Math.floor(Math.random() * 4);
+  switch(randomizer) {
+    case 0:
+      return 'colour0';
+    case 1:
+      return 'colour1';
+    case 2:
+      return 'colour2';
+    case 3:
+      return 'colour3';
+    default:
+      return 'colour0';
+  }
+}
 // Set up a callback that will run when a client connects to the server
 // When a client connects they are assigned a socket, represented by
 // the ws parameter in the callback.
 wss.on('connection', (ws) => {
   console.log('Client server has connected....');
+  ws.colour = randomColour();
+  // console.log(ws.colour);
   let userCount = wss.clients.size;
   const userJoin = {id: uuid(), type: 'postNotification', content: 'A new user has joined the room.', userCount}
   broadcast(JSON.stringify(userJoin))
+
 
   ws.on('message', (message) => {
     const messageObj = JSON.parse(message);
     messageObj.id = uuid();
     messageObj.userCount = userCount;
+    messageObj.colour = ws.colour;
+    // console.log("here is your colour: ", messageObj.colour)
     broadcast(JSON.stringify(messageObj));
   });
 
