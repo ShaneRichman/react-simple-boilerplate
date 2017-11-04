ac43ccf4bba533dd6acecde9b86d1b2f3cb66ed4
Shane Richman <Shane.M.Richman@gmail.com>
2017-11-04 20:04:36 +0000

    fixed some styling of picutres and removed the url from content

--- src/Message.jsx
+++ src/Message.jsx
@@ -4,35 +4,38 @@ import React, {Component} from 'react';
 
 class Message extends Component {
   render() {
-    // console.log(this.props.content)
+let newPost = this.props.content
 let URL = ''
-URL = this.props.content.match(/\bhttps?:\/\/\S+/gi);
-// this.props.content.replace(/^https+/, '');
-    // if (content.has(.jpg)) {
-      // return (
-        // {/*<div className="message">*/}
-          // {/*{<span className="message-username">{this.props.username}</span>}*/}
-          // {/*<span className="message-content">*/}
-            // {/*<img src="http://r.ddmcdn.com/s_f/o_1/cx_462/cy_245/cw_1349/ch_1349/w_720/APL/uploads/2015/06/caturday-shutterstock_149320799.jpg">*/}
-          // {/*</span>*/}
-        // {/*</div>*/}
-      // );
-    // {/*}*/}
+URL = newPost.match(/\bhttps?:\/\/\S+/gi);
+newPost = newPost.replace(URL, '');
+    if (URL != '') {
+      return (
+        <div className="message">
+          <span className="message-username">{this.props.username}</span>
+          <span className="message-content">
+            <p>
+            {newPost}
+            </p>
+            <img src={URL}></img>
+          </span>
+          {/*<span className="postedImg">*/}
+
+          {/*</span>*/}
+        </div>
+      );
+    }
 
     if (this.props.type === "postMessage") {
       return (
         <div className="message">
           <span className={`message-username ${this.props.colour}`}>{this.props.username}</span>
-          <span className="message-content">{this.props.content}</span>
-          <div className="postedImg">
-            <img src={URL}></img>
-          </div>
+          <span className="message-content">{newPost}</span>
         </div>
       );
     } else {
       return (
         <div className="message system">
-          {this.props.content }
+          {newPost }
         </div>
       );
     }
