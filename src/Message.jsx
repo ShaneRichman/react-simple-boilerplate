a7d42bda293bed4776ce74ba71066375b4345ef2
Shane Richman <Shane.M.Richman@gmail.com>
2017-11-06 19:23:23 +0000

    added a screen shot and fixed user color on submiting a img or gif

--- src/Message.jsx
+++ src/Message.jsx
@@ -8,36 +8,34 @@ let newPost = this.props.content
 let URL = ''
 URL = newPost.match(/\bhttps?:\/\/\S+/gi);
 newPost = newPost.replace(URL, '');
-    if (URL != '') {
+    if (URL) {
       return (
         <div className="message">
-          <span className="message-username">{this.props.username}</span>
+          <span className="message-username ${this.props.colour}">{this.props.username}</span>
           <span className="message-content">
             <p>
             {newPost}
             </p>
             <img src={URL}></img>
           </span>
-          {/*<span className="postedImg">*/}
-
-          {/*</span>*/}
-        </div>
-      );
-    }
-
-    if (this.props.type === "postMessage") {
-      return (
-        <div className="message">
-          <span className={`message-username ${this.props.colour}`}>{this.props.username}</span>
-          <span className="message-content">{newPost}</span>
         </div>
       );
     } else {
-      return (
-        <div className="message system">
-          {newPost }
-        </div>
-      );
+
+      if (this.props.type === "postMessage") {
+        return (
+          <div className="message">
+            <span className={`message-username ${this.props.colour}`}>{this.props.username}</span>
+            <span className="message-content">{newPost}</span>
+          </div>
+        );
+      } else {
+        return (
+          <div className="message system">
+            {newPost }
+          </div>
+        );
+      }
     }
 
   }
